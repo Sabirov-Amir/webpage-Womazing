3497dd4fa5bf78d8f77e809b2022b62f67717480
Sabirov Amir <superman.as13@gmail.com>
2023-07-08 17:39:54 +0300

    update popup code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 
 //-----------------------------------------------------------------------------------------------------------------------------------------
+    
+// Smooth scroll on JavaScript #1
+const pageNavigation = document.querySelectorAll('.navigation[data-goto]');
+if (pageNavigation.length > 0) {
+    pageNavigation.forEach(pageNav => {
+        pageNav.addEventListener("click", onPageNavClick);
+    });
+
+    function onPageNavClick(e) {
+        const pageNav = e.target;
+        if (pageNav.dataset.goto && document.querySelector(pageNav.dataset.goto)) {
+            const gotoBlock = document.querySelector(pageNav.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY - document.querySelector('.header').offsetHeight;
+
+            // Closing menu when click on link (inside menu)* 
+            if(headerMenu.classList.contains('active')){
+                document.body.classList.remove('lock') // Prohibit scroll when menu is open
+                headerBurger.classList.remove('active');
+                headerMenu.classList.remove('active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
+
+// * Closing menu when click on link (inside menu) - 3 strings after 'if' took from 'Menu burger on JavaScript'
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
 
 // Menu burger on JavaScript
 const headerBurger = document.querySelector('.header__burger');
