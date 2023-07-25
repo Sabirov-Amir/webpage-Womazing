9a8bb64189b23d73e375a91e1dba7182c16bc398
Sabirov Amir <superman.as13@gmail.com>
2023-07-25 21:07:24 +0300

    .team + responsive(576px) + update code

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,6 @@
 
-// Инициализируем свайпер
-const swiper = new Swiper('.about__slider-text', {
+// Swiper .about
+const swiperAbout = new Swiper('.about__slider-text', {
 
     // Optional parameters
     direction: 'horizontal',
@@ -26,5 +26,43 @@ const swiper = new Swiper('.about__slider-text', {
 
     // First slide, which we see
     initialSlide: 1,
+
+});
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+// Swiper .team 
+const swiperTeam = new Swiper('.team__slider', {
+
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation buttons
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next'
+  },
+
+  // Pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Speed
+  speed: 600,
+
+  // Number of slide for view
+  slidesPerView: 1,
+
+  // Cursor for grab
+  grabCursor: true,
+
+  // Effect 
+  effect: 'slider', // 'slider', 'fade', 'flip', 'cube', 'cards',
+
+  // First slide, which we see
+  initialSlide: 1,
 
 });
